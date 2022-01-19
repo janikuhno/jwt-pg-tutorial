7d5743bd7bb907b0ff64da68fd5c86bbd9d41d6b
Jani Kuhno <25201137+janikuhno@users.noreply.github.com>
2022-01-20 00:46:17 +0200

    added login and register

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,67 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import "./App.css";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
+// components
+import Dashboard from "./components/Dashboard";
+import Register from "./components/Register";
+import Login from "./components/Login";
+
 function App() {
-  return <Fragment></Fragment>;
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  };
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Routes>
+            <Route
+              exact
+              path="/login"
+              element={
+                !isAuthenticated ? (
+                  <Login setAuth={setAuth} />
+                ) : (
+                  <Navigate replace to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/register"
+              element={
+                !isAuthenticated ? (
+                  <Register setAuth={setAuth} />
+                ) : (
+                  <Navigate replace to="/dashboard" />
+                )
+              }
+            />
+            <Route
+              exact
+              path="/dashboard"
+              element={
+                isAuthenticated ? (
+                  <Dashboard setAuth={setAuth} />
+                ) : (
+                  <Navigate replace to="/login" />
+                )
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+    </Fragment>
+  );
 }
 
 export default App;
